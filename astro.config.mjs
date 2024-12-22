77248b79f4fedf2f6bf44a5502948055d2a43cb4
514sid <140138716+514sid@users.noreply.github.com>
2024-12-22 19:19:12 +0400

    Set site URL and add CNAME for screenlite.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://screenlite.org',
 	integrations: [
 		starlight({
 			title: 'My Docs',
